470f7d7aaedd78228aedf8426fbe81345662e191
Yannis Markonis <imarkonis@gmail.com>
2020-04-22 13:49:25 +0200

    preparing data to test analyses

---
+++ code/dev/create_artificial_rivers.R
@@ -0,0 +1,23 @@
+#Create artificial river data sets for playtesting 
+
+source('./source/libs.R')
+source('./source/graphics.R')
+
+#We shall start with no branches
+
+#type 1 are straight rivers 
+very_straight_river <- data.frame(lon = c(-1, 0, 1, 2, 3, 4, 5, 6, 7, 7, 8, 9, 10, 11, 11, 12),
+                            lat = c(5, 4, 3, 3, 2, 2, 2, 1, 0, 0, 1, 1, 0, 0, 1, 0))                          
+
+#type 2 are curved rivers 
+horse_shoe_river <- data.frame(lon = c(3, 2, 1, 2, 3, 4, 5, 6, 6, 5, 4, 3, 2, 2, 1, 0),
+                            lat = c(5, 4, 3, 3, 2, 2, 2, 1, 0, 0, 1, 1, 0, 0, 1, 0))
+
+#type 3 are the meanders
+snake_river <- data.frame(lon = c(3, 2, 1, 2, 3, 4, 5, 6, 6, 5, 4, 3, 2, 2, 1, 0),
+                          lat = c(5, 4, 3, 3, 2, 2, 2, 1, 0, 0, 1, 1, 0, 0, 1, 0))
+
+plot(very_straight_river, type = 'b')
+plot(horse_show_river, type = 'b')
+plot(snake_river, type = 'b')
+
--- code/dev/dummy.R
+++ code/dev/dummy.R
@@ -1,3 +1,14 @@
 source('./source/libs.R')
 source('./source/graphics.R')
 
+#type 1 are linear rivers with no branches
+dummy_river_1 <- data.frame(lon = c(-1, 0, 1, 2, 3, 4, 5, 6, 7, 7, 8, 9, 10, 11, 11, 12),
+                            lat = c(5, 4, 3, 3, 2, 2, 2, 1, 0, 0, 1, 1, 0, 0, 1, 0))                          
+
+#type 2 are curved rivers with no branches
+dummy_river_2 <- data.frame(lon = c(3, 2, 1, 2, 3, 4, 5, 6, 6, 5, 4, 3, 2, 2, 1, 0),
+                            lat = c(5, 4, 3, 3, 2, 2, 2, 1, 0, 0, 1, 1, 0, 0, 1, 0))
+
+plot(dummy_river_1, type = 'b')
+plot(dummy_river_2, type = 'b')
+
