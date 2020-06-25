9b52a2199da4e7156d071bded5d975b300adfc82
Yannis Markonis <imarkonis@gmail.com>
2020-06-25 13:34:40 +0200

    minor changes

--- code/dev/create_river_multiline.R
+++ code/dev/create_river_multiline.R
@@ -5,11 +5,13 @@ data_loc <- "./data/raw/hydrosheds/GEE/"
 
 filename <- paste0(data_loc, "river_pilot.shp")
 river_pilot <- st_read(filename)  
-plot(river_pilot$geometry)
 
-
-river_multi <- st_cast(river_pilot$geometry, to = 'MULTILINESTRING', ids = river_pilot$BAS_ID)
+river_multi <- st_cast(river_pilot$geometry, 
+                       to = 'MULTILINESTRING', 
+                       ids = river_pilot$BAS_ID)
 
 png('results/river_pilot_test.png')
-plot(river_multi, col = unique(river_pilot$BAS_ID), lwd = 0.5, axes =T)
+plot(river_multi, 
+     col = unique(river_pilot$BAS_ID), 
+     lwd = 0.5, axes = T)
 dev.off()
--- code/dev/get_riv_data_from_GEE.R
+++ code/dev/get_riv_data_from_GEE.R
@@ -4,22 +4,24 @@
 library(rgee)
 
 # Initialize GEE session
-ee_Initialize()
+ee_Initialize(drive = T)
 
 data_gee_id <- "WWF/HydroSHEDS/v1/FreeFlowingRivers"
 
 # define filter box
 filter_box <- ee$Geometry$Rectangle(20, 36, 25, 40)
+#filter_box <- ee$Geometry$Rectangle(20, 46, 25, 55)
 pilot_rivers <- ee$FeatureCollection(data_gee_id)$filterBounds(filter_box)
 
-task <- ee_table_to_drive(
+subset_rivers <- ee_table_to_drive(
   pilot_rivers,
   description = 'exportTable',
   folder = 'RGEE',
   fileFormat = 'SHP'
 )
-task$start()
-ee_drive_to_local(task = task,
+subset_rivers$start()
+ee_monitoring(subset_rivers) 
+ee_drive_to_local(task = subset_rivers,
                   dsn = 'data/raw/hydrosheds/GEE/river_pilot',
                   quiet = F)
 
