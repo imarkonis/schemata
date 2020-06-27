9cd4b6eed5b5c849b6a34a89b197e4e4a36c2b50
Yannis Markonis <imarkonis@gmail.com>
2020-06-27 06:35:35 +0200

    playing around

--- code/dev/create_river_multiline.R
+++ code/dev/create_river_multiline.R
@@ -15,3 +15,14 @@ plot(river_multi,
      col = unique(river_pilot$BAS_ID), 
      lwd = 0.5, axes = T)
 dev.off()
+
+one_river <- river_multi[3]
+plot(one_river, 
+     lwd = 0.5, axes = T)
+
+river_lengths <- vector()
+for(i in 1:length(river_multi)) {
+  river_lengths<- c(river_lengths,
+        length(river_multi[[i]]))
+}
+hist(river_lengths, breaks = 100)
