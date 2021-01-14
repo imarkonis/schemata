1edea91342b1fecbff88018026943f3da1202c44
Yannis Markonis <imarkonis@gmail.com>
2021-01-14 16:27:45 +0100

    starting pilot experiment

---
+++ code/dev/data_import/read_hydroshed_data.R
@@ -0,0 +1,20 @@
+## Read hydroshed DEM data
+# downloaded from https://www.dropbox.com/sh/hmpwobbz9qixxpe/AAAI_jasMJPZl_6wX6d3vEOla?dl=0
+
+source('./source/libs.R')
+library(raster)
+
+# define files
+data_loc <- "./data/raw/hydrosheds/hydrosheds_dem/"
+product <- "dem_15s_grid/"
+region <- "eu_dem_15s/"
+
+filename <- paste0(data_loc, product, region,  "w001001.adf")
+test_raster <- raster(filename)
+plot(test_raster)
+
+cellStats(test_raster, stat = max)
+
+# to convert to xyz table
+data_point <- as.data.table(rasterToPoints(test_raster))
+# Note, don't use as.data.frame, memory exceeds capacity
---
+++ code/source/pilot.R
@@ -0,0 +1,15 @@
+#Paths for data
+
+data_loc <- "./data/raw/hydrosheds/"
+product <- "hydrobasins/standard/"
+region <- "eu/"
+where <- "hybas_eu_lev01-12_v1c/"
+filename <- paste0(data_loc, product, region, where, "hybas_eu_lev10_v1c.shp")
+
+#Constants
+AREA_MIN <- 100
+AREA_MAX <- 200
+LON_MIN <- 42 
+LON_MAX <- 47 
+LAT_MIN <- 25 
+LAT_MAX <- 65 
