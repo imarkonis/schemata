e0c13bb2ae1c50e4da1930db4fd9236759fa23ca
Blöcher Johanna Ruth <bloecher@fzp.czu.cz>
2021-01-14 14:18:29 +0100

    Added asSpatialGridDataFrame option to crobbasin required for hypsometric curve.

--- code/source/geo_utils.R
+++ code/source/geo_utils.R
@@ -1,6 +1,6 @@
 #Crops basin from dem according to shape and standardizes to 0-1 elevation if needed
 
-crop_basin <- function(shape, dem, standardize = F){
+crop_basin <- function(shape, dem, standardize = F, asSpatialGridDataFrame = F){
   if(!"raster" %in% .packages()){
     library(raster)
   }
@@ -11,9 +11,13 @@ crop_basin <- function(shape, dem, standardize = F){
   shape_extent <- extent(shape)
   crop_dem <- crop(dem, shape_extent)
   mask_dem <- mask(crop_dem, shape)
-  dem_to_grid <- as(mask_dem, 'SpatialGridDataFrame')
-  dem_to_grid@data$COUNT <- mask_dem@data@values
   
+  if(asSpatialGridDataFrame){
+    dem_to_grid <- as(mask_dem, 'SpatialGridDataFrame')
+    dem_to_grid@data$COUNT <- mask_dem@data@values
+    return(dem_to_grid)
+  }
+
   if(standardize == T){
     min_eval <- min(mask_dem@data@values, na.rm = T)
     max_eval <- max(mask_dem@data@values, na.rm = T)
