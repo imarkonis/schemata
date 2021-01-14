3efe383867145d1f1328bebb89c1e97249925e70
Yannis Markonis <imarkonis@gmail.com>
2021-01-14 16:09:29 +0100

    test

--- code/dev/get_hypso_curve.R
+++ code/dev/get_hypso_curve.R
@@ -3,6 +3,7 @@ library(raster)
 library(sf)
 
 source("code/dev/calculate_rel_hypsometric_curve.R")
+source('code/source/geo_utils.R')
 
 data_loc <- "data/raw/hydrosheds/"
 product <- "hydrobasins/standard/"
@@ -20,10 +21,5 @@ product <- "hydrosheds_dem/dem_15s_grid/"
 region <- "eu_dem_15s/"
 filename <- paste0(data_loc,product, region, "w0010001.adf")
 test_dem <- raster(filename)
-shape_extent <- extent(shape)
-crop_dem <- crop(test_dem, shape_extent)
-mask_dem <- mask(crop_dem, shape)
-dem_to_grid <- as(mask_dem, 'SpatialGridDataFrame')
-dem_to_grid@data$COUNT <- mask_dem@data@values
-hyps_rel <- hypso_rel(dem_to_grid)
-  
+cropped_basin_dem <- crop_basin(shape, test_dem, asSpatialGridDataFrame = T)
+hyps_rel <- hypso_rel(cropped_basin_dem)
--- code/source/geo_utils.R
+++ code/source/geo_utils.R
@@ -1,16 +1,37 @@
 #Crops basin from dem according to shape and standardizes to 0-1 elevation if needed
 
-crop_basin <- function(shape, dem, standardize = F){
+crop_basin <- function(shape, dem, standardize = F, asSpatialGridDataFrame = F){
+  if(!"raster" %in% .packages()){
+    library(raster)
+  }
+  
+  if(!"sf" %in% .packages()){
+    library(sf)
+  }
   shape_extent <- extent(shape)
   crop_dem <- crop(dem, shape_extent)
   mask_dem <- mask(crop_dem, shape)
-  dem_to_grid <- as(mask_dem, 'SpatialGridDataFrame')
-  dem_to_grid@data$COUNT <- mask_dem@data@values
   
+  if(asSpatialGridDataFrame){
+    dem_to_grid <- as(mask_dem, 'SpatialGridDataFrame')
+    dem_to_grid@data$COUNT <- mask_dem@data@values
+    return(dem_to_grid)
+  }
+
   if(standardize == T){
     min_eval <- min(mask_dem@data@values, na.rm = T)
     max_eval <- max(mask_dem@data@values, na.rm = T)
     mask_dem@data@values <- (mask_dem@data@values-min_eval)/(max_eval-min_eval)
   }
   return(mask_dem)
+}
+
+circ_ratio <- function(shape){
+  if(!"OasisR" %in% .packages()){
+    library(OasisR)
+  }
+  perimeter_sh <- perimeter(shape)
+  area_sh <- area(shape)
+  C_dem_sm <- 4*pi*area_sh/perimeter_sh/perimeter_sh
+  return(C_dem_sm)
 }
