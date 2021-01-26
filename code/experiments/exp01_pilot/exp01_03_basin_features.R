7ff171132bea8146493e5fc689df4df9f6abf0c6
Yannis Markonis <imarkonis@gmail.com>
2021-01-26 10:12:36 +0100

    Sample features estimated

--- code/experiments/exp01_pilot/exp01_03_basin_features.R
+++ code/experiments/exp01_pilot/exp01_03_basin_features.R
@@ -1,4 +1,4 @@
-#Determine basin features 
+#Estimate basin features 
 
 source('code/source/libs.R')
 source('code/source/geo_utils.R')
@@ -14,38 +14,24 @@ basins_sf <- st_read(shapefile_basins)
 dem_raster <- raster(rasterfile_dem)
 riv_bas_ids <- readRDS(paste0("./data/experiments/", experiment, "/riv_bas_id.rds"))
 
+
 aa <- data.table(cbind(GOID = rivers_sf$GOID,
                        BB_ID = rivers_sf$BB_ID,
-             length = st_length(rivers_sf)))
-bas_feats <- riv_bas_ids[aa, on = 'GOID']
+             riv_length = st_length(rivers_sf)))
+basin_feats <- riv_bas_ids[aa, on = 'GOID']
 
 aa <- data.table(cbind(HYBAS_ID = basins_sf$HYBAS_ID,
              area = st_area(basins_sf),
              perimeter = st_length(basins_sf)))
-bas_feats <- bas_feats[aa, on = 'HYBAS_ID']
+basin_feats <- basin_feats[aa, on = 'HYBAS_ID']
 
-bas_feats[, tot_length := sum(length), by = HYBAS_ID]
-bas_feats[, gc := gc_estimate(perimeter, area)]
+basin_feats[, tot_riv_length := sum(riv_length), by = HYBAS_ID]
+basin_feats[, gc := gc_estimate(perimeter, area)]
+
+basin_feats <- unique(basin_feats[, .(HYBAS_ID, area, perimeter, tot_riv_length, gc)])
+saveRDS(basin_feats, './data/experiments/exp01/basin_feats.rds')
 
 
-rivers_sf[rivers_sf$BB_ID == 996425,]
+# Validation plots
 
-plot(rivers_sf[rivers_sf$BB_ID == 996425,]$geometry)
-
-riv_bas_ids <- data.table(cbind(HYBAS_ID = riv_bas_sf$HYBAS_ID,
-                                BB_ID = riv_bas_sf$BB_ID))
-riv_bas_ids[BB_ID == 996425]
-
-single_basin <- basins_sf[basins_sf$BB_ID == 996425, ]
-single_river <- rivers_sf[rivers_sf$BB_ID == 996425, ]
-single_dem <- crop_basin(single_basin, dem_raster)
-
-dem_dt <- data.table(rasterToPoints(single_dem))
-colnames(dem_dt) <- c('lon', 'lat', 'elevation')
-
-ggplot() +
-  #geom_raster(data = dem_dt, aes(y = lat, x = lon, fill = elevation)) +
-  geom_sf(data = single_basin, alpha = 0.1) +
-  geom_sf(data = single_river) +
-  labs(x = "", y = "") +
-  theme_light()
+plot(gc~tot_riv_length, data = basin_feats)
