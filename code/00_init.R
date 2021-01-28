a43bad151b419eb64204d355e2cb80c9f7f5f25a
Yannis Markonis <imarkonis@gmail.com>
2021-01-28 09:05:23 +0100

    New dirs and package installation

--- code/00_init.R
+++ code/00_init.R
@@ -1,15 +1,28 @@
 #First script to run 
 
+options(repos='http://cran.rstudio.org')
+have_packages <- installed.packages()
+cran_packages <- c('remotes', 'kohonen', 'randomForest','tree')
+to_install <- setdiff(cran_packages, have_packages[, 1])
+if(length(to_install)>0) install.packages(to_install)
+
+remotes::install_github("r-spatial/rgee")
+
 dir.create('./code/dev') #code under development - not yet in workflow
 dir.create('./code/archive') #code not in workflow but kept anyways
+dir.create('./code/experiments')
 dir.create('./data')
 dir.create('./data/raw') #read-only original data
 dir.create('./data/archive') #generated data that are not used in analyses
+dir.create('./data/experiments') #generated data that are not used in analyses
 dir.create('./results') 
+dir.create('./results/experiments') 
 dir.create('./results/presentations') 
 dir.create('./results/figures')
 dir.create('./results/figures/archive') #figures not used in papers/presentations
 dir.create('./docs')
 dir.create('./docs/literature')
 
-remotes::install_github("r-spatial/rgee")
+
+
+
