25aa66368894c717bdf4ce941ab06d63852085e7
imarkonis <imarkonis@gmail.com>
2021-02-10 11:49:47 +0100

    testing

--- code/00_init.R
+++ code/00_init.R
@@ -3,7 +3,7 @@
 options(repos='http://cran.rstudio.org')
 have_packages <- installed.packages()
 cran_packages <- c('remotes',  'data.table', 'tidyverse', 'dbplyr', 'foreach', 'parallel', 
-                   'sf', 'sfc', 'RPostgres', 'rpostgis', 'googledrive', 'rgee',
+                   'sf', 'sfc', 'rgdal', 'RPostgres', 'rpostgis', 'googledrive', 'rgee',
                    'kohonen', 'randomForest', 'tree', 'parallelSVM', 'spatialEco')
 to_install <- setdiff(cran_packages, have_packages[, 1])
 if(length(to_install)>0) install.packages(to_install)
