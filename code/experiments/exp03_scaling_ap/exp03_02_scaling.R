3bdfcbd6cb5aab3c68800c50103d9191d5ff110d
Yannis Markonis <imarkonis@gmail.com>
2021-10-07 09:07:52 +0200

    debugging

--- code/experiments/exp03_scaling_ap/exp03_02_scaling.R
+++ code/experiments/exp03_scaling_ap/exp03_02_scaling.R
@@ -7,13 +7,18 @@ data_files <- list.files(paste0(data_path, '/basins/level_', basin_level))
 basin_ids <- substr(data_files, 7, nchar(data_files[1]) - 4) 
 basins_n <- length(data_files)
 
+basins_225 <- st_make_valid(basins_225)
+basins_225_line <- st_cast(basins_225, "MULTILINESTRING")
+
 basins <- foreach(basin_count = 1:basins_n, .packages = c('data.table', 'sf'), 
                   .combine = 'rbind') %dopar% {
                     basin <- readRDS(paste0(data_path, 'basins/level_', basin_level, '/', data_files[basin_count]))
+                    basin <- st_make_valid(basin)
+                    basin_line <- st_cast(basin, "MULTILINESTRING")
                     data.table(pfaf_id = factor(basin_ids[basin_count]),
                                region = basin$region,
                                area = as.numeric(st_area(basin)),
-                               perimeter = as.numeric(st_length(basin)))
+                               perimeter = as.numeric(st_length(basin_line)))
                   }
 basins <- unique(basins[complete.cases(basins)])
 saveRDS(basins, paste0(data_path, 'basin_feats_', basin_level, '.rds'))
