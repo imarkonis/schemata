a87122b3b4ad6eeebbca84f001b740c462afff54
johanna <reintheoretisch@gmail.com>
2022-02-15 17:59:57 +0100

    Updating experiment 5

--- code/experiments/exp05_NCI/exp05_01_import_data.R
+++ code/experiments/exp05_NCI/exp05_01_import_data.R
@@ -15,7 +15,8 @@ passwordn <- rstudioapi::askForPassword("Database password")
 dsn <- paste0("PG:dbname='earth' host=localhost user=", usern," password=",passwordn," port=5432 schema='basin_dem' table='eu_dem_15s_grid' mode=2")
 rasterfile_dem <- readGDAL(dsn) # Get your file as SpatialGridDataFrame
 dem_raster <- raster(rasterfile_dem)
-
+#save(dem_raster, file = "dem_raster.Rdata")
+#load("dem_raster.Rdata")
 rm(list=c("usern", "passwordn", "dsn", "rasterfile_dem"))
 
 # Find Main River 
@@ -31,41 +32,23 @@ schema_tables <- dbGetQuery(con, "SELECT table_name FROM information_schema.tabl
 query_riv_allinfo <- paste0("SELECT * FROM river_atlas.", schema_tables$table_name[5])
 
 region_rivall <- st_read(con, query = query_riv_allinfo)
-
+#save(region_rivall, file = "region_rivall.Rdata")
+load("region_rivall.Rdata")
 
 dt <- data.table(region_rivall)
 dt_oc <- dt[ord_clas == 1]
-st <- subset(dt_oc, select = c(gid, hyriv_id, next_down, main_riv, length_km, dist_dn_km, dist_up_km, ord_clas))
+st <- subset(dt_oc, select = c(gid, hyriv_id, next_down, main_riv, length_km, dist_dn_km, dist_up_km, ord_clas, hybas_l12))
 st[, segments_count := nrow(.SD), main_riv]
 st_sel <- st[segments_count > 100]
 
 
-get_xy <- function(hyriv_id){
-  print(hyriv_id)
-  ext <- extent(subset(region_rivall, hyriv_id == hyriv_id))
-  x <- mean(ext[1:2])
-  y <- mean(ext[3:4])
-  return(c(x,y))
-}
+hyriv_ids <- st_sel$hyriv_id 
 
-xy <- mclapply(st_sel$hyriv_id, FUN = get_xy, mc.cores = cores_n)
+if(.Platform$OS.type == "unix") { 
+  river_xyz <- mclapply(hyriv_ids, FUN = get_xyz, mc.cores = cores_n)
+} else {
+  river_xyz <- mclapply(hyriv_ids, FUN = get_xyz, mc.cores = 1)
+ }
 
-st_sel[, extent(subset(region_rivall, hyriv_id == hyriv_id))[1:2],hyriv_id]
-st_sel[, y := mean(extent(subset(region_rivall, hyriv_id == hyriv_id))[3:4]),hyriv_id]
+river_xyz_dt<- as.data.table(do.call(rbind, river_xyz))
 
-# make mean x, y for each segment
-
-#st_sel[, z_val:=  ]
-main_river <- unique(st_sel$main_riv)
-ex_dem <- extent(dem_raster)
-intersect(ex_dem, region_riv_main)
-
-main_river_sel <- main_river[1]
-region_riv_main <- subset(region_rivall, hyriv_id == main_river_sel)
-dem_crop <- crop_basin(region_riv_main, dem_raster)
-
-plot(region_riv_main$geom)
-plot(dem_raster)
-crop_dem <- crop_basin(region_riv_main, dem_raster)
-min(!is.na(crop_dem@data@values))
-max(!is.na(crop_dem@data@values))
--- code/source/experiments/exp_05.R
+++ code/source/experiments/exp_05.R
@@ -19,3 +19,20 @@ registerDoParallel(cores = cores_n - 2)
 
 # Constants
 
+# Functions
+get_xyz <- function(hyriv_id_sel){
+  riv_sel <- subset(region_rivall, hyriv_id == hyriv_id_sel)
+  riv_sel_geom <- st_geometry(riv_sel)
+  riv_sel_geom_xy <- as.data.table(riv_sel_geom[[1]][[1]])
+  names(riv_sel_geom_xy) = c("x", "y")
+  
+  riv_sel_dt  <-  data.table(riv_sel)
+  riv_sel_dt <- subset(riv_sel_dt, select = c(gid, hyriv_id, next_down, main_riv, length_km, dist_dn_km, dist_up_km, ord_clas))
+  
+  riv_sel_xy <- cbind(data.table(riv_sel_dt), riv_sel_geom_xy)
+  z <- extract(x = dem_raster, y = riv_sel_geom[[1]][[1]])
+  riv_sel_xy[, z:= z]
+  return(riv_sel_xy)
+}
+
+
