24965dbf09dea377b625e0605450efbaa595d074
Yannis Markonis <imarkonis@gmail.com>
2022-04-27 19:18:27 +0200

    same

--- code/experiments/exp06_hyper_resolution/exp06_03_aggregate.R
+++ code/experiments/exp06_hyper_resolution/exp06_03_aggregate.R
@@ -15,7 +15,7 @@ dem_files_png <- list.files(data_source_path, pattern = "*dem.png")
 pal <- colorRampPalette(c("white", "black"))
 
 length(dem_files_png)
-foreach(file_count = 1:5, .packages = c('data.table', 'sf', 'png')) %dopar% {
+foreach(file_count = 50001:length(dem_files_png), .packages = c('data.table', 'sf', 'png')) %dopar% {
   dem_raster <- raster(paste0(data_source_path, dem_files_png[file_count]))
 
   png(paste0(data_save_path, 'scale_1/', dem_files_png[file_count]))
@@ -50,17 +50,4 @@ par(mar = c(0,0,0,0))
 plot(aggregate(dem_raster, 8), col = rev(pal(100)),  key.pos = NULL, legend = FALSE, axes=FALSE, box = FALSE)
 
 
-
-res <- 0.5
-lon_cuts <- seq(dem_raster@extent@xmin, dem_raster@extent@xmax, res)
-lat_cuts <- seq(dem_raster@extent@ymin, dem_raster@extent@ymax, res)
-
-for(lon_count in 1:(length(lon_cuts) - 1)){
-  for(lat_count in 1:(length(lat_cuts) - 1)){
-    e <- extent(lon_cuts[lon_count], 
-                lon_cuts[lon_count + 1],
-                lon_cuts[lon_count],
-                lat_cuts[lat_count + 1])
-    dem_raster_subset <- crop(dem_raster, e)
-    writeRaster(dem_raster_subset, paste0(data_path, '/dem_', lon_cuts[lon_count], '_', lon_cuts[lon_count]))
 
