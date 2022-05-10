c3e7e51295c44ba19b32ced71f0f798d3332507a
Blöcher Johanna Ruth <bloecher@fzp.czu.cz>
2022-05-10 13:59:36 +0200

    Getting more points on hyriv lines

--- code/experiments/exp05_NCI/exp05_01_a_import_river_get_npoints_xy_global.R
+++ code/experiments/exp05_NCI/exp05_01_a_import_river_get_npoints_xy_global.R
@@ -16,6 +16,7 @@ con <- dbConnect(Postgres(), dbname = db_name, host = host_ip, port = port_n,
 
 schema_tables_rivers <- dbGetQuery(con, "SELECT table_name FROM information_schema.tables WHERE table_schema = 'river_atlas'") 
 
+
 for(i in 1:length(schema_tables_rivers$table_name)){
   print(schema_tables_rivers$table_name[i])
   query_riv_allinfo <- paste0("SELECT gid, hybas_l12, hyriv_id, main_riv, next_down, length_km, dist_dn_km, dist_up_km, ord_clas, geom FROM river_atlas.", schema_tables_rivers$table_name[i])
@@ -27,15 +28,21 @@ for(i in 1:length(schema_tables_rivers$table_name)){
   rm(region_rivall)
   coords_dt <- data.table(coords)
   rm(coords)
+  coords_more_points <- coords_dt[,CreateSegments_coords(coords = cbind(.SD$X, .SD$Y), n.parts = 10), by = L2]
+  names(coords_more_points) <- c("L2", "X", "Y")  
+  coords_dt <- coords_more_points
+  rm(coords_more_points)
   coords_dt[,X2:= data.table::shift(X, n = 1, type = "lag", fill = X[1]), by = L2]
   coords_dt[,Y2:= data.table::shift(Y, n = 1, type = "lag", fill = Y[1]), by = L2]
   coords_dt$id <- 1:nrow(coords_dt)
-  coords_dt[,distance:=st_distance(st_sfc(st_point(x= c(.SD$X,.SD$Y)), crs = crs_region$input),st_sfc(st_point(x= c(.SD$X2,.SD$Y2)), crs = crs_region$input), by_element = T),id]
-  coords_dt[,cum_dist:= cumsum(distance), L2]
+  coords_dt[,distance :=st_distance(st_sfc(st_point(x= c(.SD$X,.SD$Y)), crs = crs_region$input),st_sfc(st_point(x= c(.SD$X2,.SD$Y2)), crs = crs_region$input), by_element = T),id]
+  coords_dt[,cum_dist := cumsum(distance), L2]
+  river_xyz[,cum_dist_v2 := rev(cum_dist) , L2]  
   print("got coords")
   test <- merge(region_dt, coords_dt, by.x = "gid", by.y = "L2", all.y = T)
   test <- data.table(test)
-  test[, dist_up_km_detailed := dist_up_km + as.numeric(cum_dist)/1000]
+  test[, dist_dn_km_detailed := dist_dn_km + as.numeric(cum_dist_v2)/1000]
+  test[, dist_up_km_detailed := dist_up_km - as.numeric(cum_dist_v2)/1000]
   test[, L1:= NULL]
   test[, X2:= NULL]
   test[, Y2:= NULL]
