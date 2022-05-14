9cf4a22e59c91465e5d3018bfe6dcb3ae79340b9
Markonis Ioannis <markonis@fzp.czu.cz>
2022-05-14 10:04:20 +0200

    testing new git connection

--- code/@sql_examples.R
+++ code/@sql_examples.R
@@ -1,2 +1,4 @@
 dbGetQuery(con, "SELECT nspname FROM pg_catalog.pg_namespace") #Check schemas
 DBI::dbListObjects(con, DBI::Id(schema = 'basin_boundaries')) #Check tables of a schema
+
+#A comment from my new notebook
