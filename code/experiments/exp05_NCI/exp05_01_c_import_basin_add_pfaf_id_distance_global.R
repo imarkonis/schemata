0c4036aa6352dca760c1dbe3492dbc8ed6c018fa
jorub <bloecher@fzp.czu.cz>
2022-05-16 14:18:23 +0200

    fixing si

--- code/experiments/exp05_NCI/exp05_01_c_import_basin_add_pfaf_id_distance_global.R
+++ code/experiments/exp05_NCI/exp05_01_c_import_basin_add_pfaf_id_distance_global.R
@@ -6,20 +6,12 @@ source('code/source/database.R')
 source('code/source/geo_functions.R')
 source('code/source/experiments/exp_05.R')
 
-library(RPostgres)
-
-con <- dbConnect(Postgres(), dbname = db_name, host = host_ip, port = port_n,        
-                 user = rstudioapi::askForPassword("Database user"),      
-                 password = rstudioapi::askForPassword("Database password"))
-
 regions <- c("af", "as", "na", "au", "eu", "si", "sa_n", "sa_s")
 
 for(i in 1:length(regions)){
   river_xyz <- readRDS(paste0(data_path,'/z_',regions[i], '_rivers_xy_dist.rds'))
   name_merge <- paste0(data_path,'/',regions[i], '_rivers_xyz_pfaf_distance.rds')
-  if( i %in% c(6)){
-    regions[i] = "as"
-  }
+  
   if( i %in% c(7,8)){
     regions[i] = "sa"
   }
